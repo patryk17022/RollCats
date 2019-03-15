f7d67482b5e1fde8a43942910f7142a1ef9730d7
patryk17022 <patrykgalaska@gmail.com>
2019-03-15 21:13:13 +0100

    WIN

--- example.js
+++ example.js
@@ -1,3 +1,6 @@
+var controller = new EGZOController();
+
+
 
 var Engine = Matter.Engine,
 Render = Matter.Render,
@@ -13,13 +16,27 @@ element: document.body,
 engine: engine
 });
 
-// create two boxes and a ground
+var Body = Matter.Body;
 var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
+var posx = 0;
+controller.onvalue = function(value)
+{
+
+    if(value > 0.5){
+        posx++;
+
+    }else if (value < -0.5){
+        posx--;
+    }
+
+    Body.translate( boxA, {x: posx, y: 0});
+    Body.rotate(boxA,posx*2*3.14/360);
+};
+
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [boxA,ground]);
 
 // run the engine
 Engine.run(engine);
