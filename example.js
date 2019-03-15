60a2ae8637c38cdac83139d80762097f5e80b9e0
Ben Ghazi <michal.t.kret@gmail.com>
2019-03-15 22:43:12 +0100

    sledzenie kulki

--- example.js
+++ example.js
@@ -19,7 +19,8 @@ engine: engine,
 options: {
     width: window.innerWidth,
     height: window.innerHeight,
-    wireframes: false
+    wireframes: false,
+    hasBounds: true
 }
 });
 
@@ -27,7 +28,27 @@ var Body = Matter.Body;
 var boxA = Bodies.circle(400, 200, 80);
 var posx = 0;
 var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight - 30, window.innerWidth, 60, { isStatic: true });
+World.add(engine.world, boxA);
 World.add(engine.world, ground);
+
+Matter.Events.on(this.engine, 'beforeTick', function() {
+     
+        // apply zoom
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext("2d");
+        ctx.translate(window.innerWidth/2, window.innerHeight/2);
+        ctx.scale(this.zoom, this.zoom);
+        ctx.translate(-window.innerWidth/2, -window.innerHeight/2);  
+
+    // center view at player 
+    Matter.Bounds.shift(render.bounds,
+    {
+        x: boxA.position.x - window.innerWidth / 2,
+        y: boxA.position.y - window.innerHeight / 2
+    });
+
+}.bind(this));
+
 controller.onvalue = function(value)
 {
     var valueDiff = value - currentValue;
@@ -38,8 +59,6 @@ controller.onvalue = function(value)
     Matter.Composite.rotate( engine.world, rotationValue, {x: window.innerWidth/2, y: window.innerHeight/2});
 };
 
-// add all of the bodies to the world
-World.add(engine.world, [boxA,ground]);
 
 // run the engine
 Engine.run(engine);
