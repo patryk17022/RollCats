6a302b381c2e9b6c8672e45ae1e8adc5367b53c2
patryk17022 <patrykgalaska@gmail.com>
2019-03-16 00:06:18 +0100

    Kurna TOmasz

--- tomasz.js
+++ tomasz.js
@@ -1,6 +1,11 @@
 var controller = new EGZOController();
 
-
+class Position {
+    constructor(wysokosc, szerokosc) {
+      this.x=wysokosc;
+      this.y=szerokosc;
+    }
+  }
 
 var Engine = Matter.Engine,
 Render = Matter.Render,
@@ -21,18 +26,33 @@ options: {
 }
 });
 
+function bezier(p1, p2,p3,a)
+{
+    var Px=p1.x*(1-a)*(1-a)+p2.x*a*(1-a)+2*p3.x*a;
+var Py=p1.y*(1-a)*(1-a)+p2.y*a*(1-a)+2*p3.y*a;
+    return new Position(Px,Py);
+}
+
 var Body = Matter.Body;
 var boxA = Bodies.rectangle(400, 200, 80, 80);
 
+var p1= new Position(300,100);
+var p2 = new Position(400,400);
+var p3 = new Position(500,100);
+var bezierTable=[p1,p2,p3,new Position(500,100),new Position(600,0),new Position(700,100)];
 
-var table=[Bodies.rectangle(600, 100, 100, 20, { isStatic: true,angle:-40 })];
+var tmpTable=[p1,p2];
 
-for(var a=0;a<10;a++)
-{
-    table.push(Bodies.rectangle(600, 100+a*20, 100, 20, { isStatic: true,angle:-40 }))
-}
 
-var tmptable=[];
+
+var table=[Bodies.rectangle(p1.x,p1.y, 100, 20, { isStatic: true,angle:-40 })];
+
+// for(var a=0;a<10;a++)
+// {
+//     table.push(Bodies.rectangle(600, 100+a*20, 100, 20, { isStatic: true,angle:-40 }))
+// }
+
+
 
 
 
@@ -44,11 +64,30 @@ var tmptable=[];
 //V.x/length
 //V.y/length
 //
+var Px=0;
+var Py=0;
 
-var beginPoint = P(0)
+var Ppx=0;
+var Ppy=0;
+var len;
+
+var Vx;
+var Vy;
+
+var circleTab=[];
+//var beginPoint = P(0)
+for(var b=0;b<bezierTable.length/3;b+=3)
 for(var a = 0; a <=1.0; a += 0.1)
-{
 
+{
+// Px=p1.x*(1-a)*(1-a)+p2.x*a*(1-a)+2*p3.x*a;
+// Py=p1.y*(1-a)*(1-a)+p2.y*a*(1-a)+2*p3.y*a;
+// Ppx=2*p1.x*a-2*p1.x+2*p2.x*a-p2.x+2*p3.x*a;
+// Ppy=2*p1.y*a-2*p1.y+2*p2.y*a-p2.y+2*p3.y*a;
+// len=Math.sqrt(Ppx*Ppx+Ppy*Ppy);
+//circleTab.push(Bodies.circle(Px,Py,10, { isStatic: true}));
+var tmpPos=bezier(bezierTable[b],bezierTable[b+1],bezierTable[b+2],a);
+circleTab.push(Bodies.circle(tmpPos.x,tmpPos.y,10, { isStatic: true}));
 }
 
  //var wall1 =Bodies.rectangle(400, 350, 100, 20, { isStatic: true,angle:-40 });
@@ -86,6 +125,7 @@ var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
 // add all of the bodies to the world
 World.add(engine.world, [boxA,ground,table[0]]);
+for(var a=0;a<20;a++)World.add(engine.world, [circleTab[a]]);
 
 
 // run the engine
